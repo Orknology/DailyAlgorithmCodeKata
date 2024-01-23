33c344883e7474201a52328f94167ad09fb8d472
Orknology <122108152+Orknology@users.noreply.github.com>
2024-01-23 09:13:49 +0900

    없는 숫자 더하기.cs

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -5,5 +5,8 @@ public class Solution {
     public int solution(int[] numbers) {
         int answer = 45 - numbers.Sum();
         return answer;
+    //혹은
+    //var numberArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //return numberArray.Except(numbers).Sum();
     }
 }
