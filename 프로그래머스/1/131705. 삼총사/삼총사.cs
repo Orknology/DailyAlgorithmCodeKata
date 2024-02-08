fbe38f562c89ac1a4af6fd7bc484ec56abaad317
Orknology <122108152+Orknology@users.noreply.github.com>
2024-02-08 09:27:50 +0900

    [level 1] Title: 삼총사, Time: 0.20 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cs
+++ 프로그래머스/1/131705. 삼총사/삼총사.cs
@@ -8,12 +8,12 @@ public class Solution {
         int answer = 0;
         
         for(int i = 0; i < number.Length; i++)
-            for(int j = 1; j < number.Length; j++)
-                for(int k = 2; k < number.Length; k++)
+            for(int j = i + 1; j < number.Length; j++)
+                for(int k = j + 1; k < number.Length; k++)
                     //i < j < k로 같은 수 제외 및 크기 구분
-                    if(i < j && j < k)
-                        if((number[i] + number[j] + number[k]) == 0)
-                            answer++;
+                    //if(i < j && j < k)
+                    if((number[i] + number[j] + number[k]) == 0)
+                        answer++;
         
         return answer;
     }
