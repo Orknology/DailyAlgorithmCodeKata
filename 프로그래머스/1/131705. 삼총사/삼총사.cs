a2d6388270434743615fdfda04d6879429deefc3
Orknology <122108152+Orknology@users.noreply.github.com>
2024-02-08 09:28:52 +0900

    Update 삼총사.cs

--- 프로그래머스/1/131705. 삼총사/삼총사.cs
+++ 프로그래머스/1/131705. 삼총사/삼총사.cs
@@ -10,8 +10,6 @@ public class Solution {
         for(int i = 0; i < number.Length; i++)
             for(int j = i + 1; j < number.Length; j++)
                 for(int k = j + 1; k < number.Length; k++)
-                    //i < j < k로 같은 수 제외 및 크기 구분
-                    //if(i < j && j < k)
                     if((number[i] + number[j] + number[k]) == 0)
                         answer++;
         
